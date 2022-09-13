dcc7af6aab1ebc081edf5f33ec0657b684fd1f3f
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-09-13 11:50:26 -0300

    ejemplo express

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hola mundo");
+//npx nodemon index
+//extension rest client
+
+//requerir modulo express y gardarlo en variable (es una funcion)
+const express = require('express');
+//llamamos a funcion express
+const app = express();
+//escuchamos a puerto
+const port = 9000;
+
+//llamamos a app y cuando tenga ruta /, llama a la funcion
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+//escuche a puerto y cuando se prende que corra esto
+app.listen(port, () => {
+    console.log (`> server running on port ${port}`);
+});
+
+
+
+
